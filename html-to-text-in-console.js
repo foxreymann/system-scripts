1b3487f1f0590244cdde97baf94fbb7626361cf7
foxreymann <foxreymann@gmail.com>
2016-04-13 14:25:56 +0100

    scrape html to text from website in console script

---
+++ html-to-text-in-console.js
@@ -0,0 +1,28 @@
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+(function($){
+   $.fn.innerText = function(msg) {
+         if (msg) {
+            if (document.body.innerText) {
+               for (var i in this) {
+                  this[i].innerText = msg;
+               }
+            } else {
+               for (var i in this) {
+                  this[i].innerHTML.replace(/&amp;lt;br&amp;gt;/gi,"n").replace(/(&amp;lt;([^&amp;gt;]+)&amp;gt;)/gi, "");
+               }
+            }
+            return this;
+         } else {
+            if (document.body.innerText) {
+               return this[0].innerText;
+            } else {
+               return this[0].innerHTML.replace(/&amp;lt;br&amp;gt;/gi,"n").replace(/(&amp;lt;([^&amp;gt;]+)&amp;gt;)/gi, "");
+            }
+         }
+   };
+})(jQuery);
+
+copy($('.post_content').innerText())
